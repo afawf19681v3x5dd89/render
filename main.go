ac3e696f9c0b807918a0da35be40f817d035f294
admin <admin>
2023-03-22 17:44:21 +0300

    change address and port

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 		w.Write([]byte("asdasd"))
 	})
 
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe("0.0.0.0:8000", nil)
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
