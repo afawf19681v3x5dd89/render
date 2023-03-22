0f6d0306b8f9a4c17e66b3369120fd8708a1fcc8
admin <admin>
2023-03-22 17:51:11 +0300

    add handler

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 		w.Write([]byte("asdasd"))
 	})
 
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("текст"))
+	})
+
 	err := http.ListenAndServe("0.0.0.0:8000", nil)
 	if err != nil {
 		log.Fatalf("error: %s", err)
